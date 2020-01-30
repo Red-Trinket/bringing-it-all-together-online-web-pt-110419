e8b781a1253997c5f0c9949c93404c3aacfe57c3
Matthew Jones <matts3371@gmail.com>
2020-01-30 20:45:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,10 @@ class Dog
   end 
   
   def new_from_db(row)
-    new_song = 
+    new_dog = self.new 
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
   
   def save 
     sql = <<-SQL
